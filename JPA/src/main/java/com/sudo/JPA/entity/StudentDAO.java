133683b039d0ea7ecdfb43b7ef29fbfb9e96f658
zahid-01 <erzahidkhan101@gmail.com>
2023-09-20 20:34:39 +0530

    CRUD in progress

--- JPA/src/main/java/com/sudo/JPA/entity/StudentDAO.java
+++ JPA/src/main/java/com/sudo/JPA/entity/StudentDAO.java
@@ -1,5 +1,13 @@
 package com.sudo.JPA.entity;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
+    Student retrieve(int id);
+
+    List<Student> getAll();
+
+    List<Student> getByLastName(String lastName);
+    List<Student> predicateOr(String lastName);
 }
--- JPA/src/main/java/com/sudo/JPA/entity/StudentDAOImpl.java
+++ JPA/src/main/java/com/sudo/JPA/entity/StudentDAOImpl.java
@@ -1,21 +1,52 @@
 package com.sudo.JPA.entity;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
-public class StudentDAOImpl implements StudentDAO{
+public class StudentDAOImpl implements StudentDAO {
 
     private final EntityManager entityManager;
 
     @Autowired
-    StudentDAOImpl(EntityManager entityManager){
+    StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
     @Transactional
-    public void save(Student theStudent){
+    public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    @Override
+//    @Transactional
+    public Student retrieve(int id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> getAll() {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> getByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName = :lastName",Student.class);
+        query.setParameter("lastName", lastName);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> predicateOr(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("from Student where firstName = 'jakarta' OR firstName = 'nimda'", Student.class);
+
+        return query.getResultList();
     }
 }
