9e98d52a438f9f65df91dc9da2bc517cb0205684
zahid-01 <erzahidkhan101@gmail.com>
2023-09-25 09:22:12 +0530

    "Initial Commit"

--- REST API's/Spring Data REST/src/main/java/com/sudo/dev/Employees/dao/EmployeeRepository.java
+++ REST API's/Spring Data REST/src/main/java/com/sudo/dev/Employees/dao/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.sudo.dev.Employees.dao;
 
 import com.sudo.dev.Employees.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Controller;
 
+
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
